e1537e02ef7639324f1aa25857df1bf97cc387f6
Geoffrey Jones <jonesg5@rpi.edu>
2018-01-23 13:13:39 -0500

    Last Commit

--- CSbatch.m
+++ CSbatch.m
@@ -1,11 +1,11 @@
-function CSbatch
+function results = CSbatch
 %CSBATCH find the CS from SPD of all .isd files in a folder
 
 addpath('CS_CLA_postBerlinCorrMelanopsin_02Oct2012');
 
 %% Have user select folder to load
 startDir = fullfile([filesep,filesep,'ROOT'],'projects',...
-    'NASA-Review','2014-01-22_spectrometerReadings');
+    'NASA-Review');
 fileDir = uigetdir(startDir,'Select folder to analyze');
 dirName = regexprep(fileDir,'^.*[^\w-](\w*)$','$1');
 calFile = fullfile(startDir,'cal.mat');
@@ -16,18 +16,22 @@ dirListing = dir(fullfile(fileDir,'*.isd'));
 %% Import and process all files
 nFiles = numel(dirListing);
 % Preallocate variables
+results = dataset;
+results.condition = cell(nFiles,1);
 filePath = cell(nFiles,1);
 wavelength = cell(nFiles,1);
 irradiance = cell(nFiles,1);
 spd = cell(nFiles,1);
-Lux = zeros(nFiles,1);
-x = zeros(nFiles,1);
-y = zeros(nFiles,1);
-CLA = zeros(nFiles,1);
-CS = zeros(nFiles,1);
-sampleTime = zeros(nFiles,1);
+results.Lux = zeros(nFiles,1);
+results.x = zeros(nFiles,1);
+results.y = zeros(nFiles,1);
+results.CCT = zeros(nFiles,1);
+results.CLA = zeros(nFiles,1);
+results.CS = zeros(nFiles,1);
 % Begin loop
 for i1 = 1:nFiles
+    % Set results.condition name
+    results.condition{i1} = dirListing(i1).name(1:end-4);
     % Import the current file
     filePath{i1} = fullfile(fileDir,dirListing(i1).name);
     [wavelength{i1},irradiance{i1}] = importISD(filePath{i1});
@@ -36,23 +40,11 @@ for i1 = 1:nFiles
     % Merge SPD
     spd{i1} = [wavelength{i1}(:),irradiance{i1}(:)];
     % Perform analysis
-    [Lux(i1),x(i1),y(i1)] = Lxy23Sep05(spd{i1});
-    CLA(i1) = CLA_postBerlinCorrMelanopsin_02Oct2012(spd{i1});
-    CS(i1) = CSCalc_postBerlin_12Aug2011(CLA(i1));
-    % Extract sample time
-    sampleTime(i1) = datenum(dirListing(i1).date);
+    [results.Lux(i1),results.x(i1),results.y(i1)] = Lxy23Sep05(spd{i1});
+    results.CCT(i1) = CCT23Sep05(spd{i1});
+    results.CLA(i1) = CLA_postBerlinCorrMelanopsin_02Oct2012(spd{i1});
+    results.CS(i1) = CSCalc_postBerlin_12Aug2011(results.CLA(i1));
 end
-
-%% Display results
-figure;
-hold on
-[AX,~,~] = plotyy(sampleTime,CS,sampleTime,[Lux,CLA],'plot','semilogy');
-datetick2;
-title(dirName);
-xlabel('sample time');
-set(get(AX(1),'Ylabel'),'String','Circadian Stimulus (CS)') 
-set(get(AX(2),'Ylabel'),'String','Illuminance (Lux)')
-legend('CS','Lux','CLA','Location','SouthOutside','Orientation','Horizontal');
 
 end
 
